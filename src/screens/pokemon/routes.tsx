7a6e3051780a9edb574bc2b49857fe3b984f926c
Carlos Molina <47950385+cmyugovic@users.noreply.github.com>
2021-01-15 16:52:47 -0300

    Change pokemon route

--- src/screens/pokemon/routes.tsx
+++ src/screens/pokemon/routes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PokemonList from "./list/List";
 const routes: any = {
-  path: "/pokemon",
+  path: "/",
   children: [{ path: "/", element: <PokemonList /> }],
 };
 export default routes;
